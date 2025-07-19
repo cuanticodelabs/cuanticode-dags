355280cd3221bdba77442ac6a96ddf6e5b507e1d
HectorGonzalez <87376866+hectogonzalezb@users.noreply.github.com>
2025-07-19 19:19:30 -0400

    ini

---
+++ miotrodags.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='chicoagarrame_este_dag',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=['example'],
+) as dag:
+    print_hello = BashOperator(
+        task_id='print_hello',
+        bash_command='echo "¡Hola desde Airflow!"',
+    )
